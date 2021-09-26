5baddd041788fc91e56554bae35059960fc89c65
David Morgenstern <DavidtheConfident@gmail.com>
2021-09-26 16:42:58 -0500

    commit tree 6261bdab02b177be279485a9f2fa8cc173fdaf71

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+
+//let short = [...currentUser]
+let short = currentUser.slice()
+
+const shortGreeting = `Welcome, ${short[0]}!`
+
